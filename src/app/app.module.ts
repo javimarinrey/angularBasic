05c883107eaa6822183ec715af1a21263cf54d15
javimarinrey <javimarinrey@gmail.com>
2017-12-12 22:17:31 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule} from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
+import { TranslateModule } from 'ng2-translate';
 
 
 // Rutas
@@ -15,6 +17,8 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { PipesComponent } from './components/tutorial/pipes/pipes.component';
 import { Ng2chartsComponent } from './components/modulos/ng2charts/ng2charts.component';
+import { EmpezandoComponent } from './components/empezando/empezando.component';
+import { Ng2translateComponent } from './components/modulos/ng2translate/ng2translate.component';
 
 
 
@@ -27,12 +31,16 @@ import { Ng2chartsComponent } from './components/modulos/ng2charts/ng2charts.com
     NavbarComponent,
     PipesComponent,
     CapitalizadoPipe,
-    Ng2chartsComponent
+    Ng2chartsComponent,
+    EmpezandoComponent,
+    Ng2translateComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     APP_ROUTING,
-    ChartsModule
+    ChartsModule,
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
