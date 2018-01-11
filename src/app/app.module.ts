41fc45ab38cc6dd97e2546eba571ea3ea89d2c08
uge01011 <javier.marin@hotusa.com>
2018-01-11 13:10:04 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,7 +46,6 @@ import { TimepickerComponent } from './components/modulos/bootstrap4/timepicker/
 import { TooltipComponent } from './components/modulos/bootstrap4/tooltip/tooltip.component';
 import { TypeaheadComponent } from './components/modulos/bootstrap4/typeahead/typeahead.component';
 import { Ng2animateComponent } from './components/modulos/ng2animate/ng2animate.component';
-import { Ng2nouisliderComponent } from './components/modulos/ng2nouislider/ng2nouislider.component';
 
 
 
@@ -81,8 +80,7 @@ import { Ng2nouisliderComponent } from './components/modulos/ng2nouislider/ng2no
     TimepickerComponent,
     TooltipComponent,
     TypeaheadComponent,
-    Ng2animateComponent,
-    Ng2nouisliderComponent
+    Ng2animateComponent
   ],
   imports: [
     BrowserModule,
